dbec57eac9bb898ad3066e2609fd4dd4f44efaf1
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-08-23 07:57:24 -0400

    Updated Formating of Temperature Read out

--- tempscript.py
+++ tempscript.py
@@ -3,12 +3,14 @@ import sys
 from colorama import Fore, Style
 
 def colorTemp(coreTemp):
+    degree_sign = u'\N{DEGREE SIGN}'
+
     if coreTemp <= 59:
-        print(f'{Fore.GREEN}' + str(coreTemp),end='\r')
+        print(f'CPU: {Fore.GREEN}' + str(coreTemp) + u'\N{DEGREE SIGN}' + 'C',end='\r')
     if coreTemp >= 60 and coreTemp <= 79:
-        print(f'{Fore.YELLOW}' + str(coreTemp),end='\r')
+        print(f'{Fore.YELLOW}' + str(coreTemp) + u'\N{DEGREE SIGN}' + 'C',end='\r')
     if coreTemp >= 80:
-        print(f'{Fore.RED}' + str(coreTemp),end='\r')
+        print(f'{Fore.RED}' + str(coreTemp) + u'\N{DEGREE SIGN}' + 'C',end='\r')
 
 def main():
     while(True):
