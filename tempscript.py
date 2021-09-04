7d9281b20fb823eb68d0269a0ed6bbbd5508f7d4
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-09-04 11:48:56 -0400

    Removed Redundant While Loop

--- tempscript.py
+++ tempscript.py
@@ -13,17 +13,15 @@ def colorTemp(coreTemp):
         print(f'CPU {Fore.RED}' + str(coreTemp) + u'\N{DEGREE SIGN}' + 'C',end='\r')
 
 def main():
-    while(True):
-        core0 = open("/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp2_input")
-        core1 = open("/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp3_input")
-        core0Temp = int(core0.read())
-        core1Temp = int(core1.read())
+    core0 = open("/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp2_input")
+    core1 = open("/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp3_input")
+    core0Temp = int(core0.read())
+    core1Temp = int(core1.read())
 
-        coreAvgTemp = (core0Temp + core1Temp) / 2
-        colorTemp(coreAvgTemp / 1000)
-
-        sys.stdout.flush()
-        time.sleep(3)
+    coreAvgTemp = (core0Temp + core1Temp) / 2
+    colorTemp(coreAvgTemp / 1000)
+    sys.stdout.flush()
+    time.sleep(1)
     
 if __name__ == "__main__":
     main()
